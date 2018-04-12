71179ab25bb9d6f7270721f7c1d0b799ed15f8b5
shalun002 <32149071+shalun002@users.noreply.github.com>
2018-04-12 11:49:05 +0600

    First commit

--- Lessons/src/Lessons/Input.java
+++ Lessons/src/Lessons/Input.java
@@ -1,4 +1,13 @@
 package Lessons;
 
+import java.util.Scanner;
+
 public class Input {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите что-нибудь");
+        String str = scanner.nextLine();
+
+        System.out.println("Вы ввели " + str);
+    }
 }
