66be23fba85c87c7b58c7535a11a93f757f60bda
shalun002 <32149071+shalun002@users.noreply.github.com>
2018-04-17 19:20:38 +0600

    Override

---
+++ Lessons/src/Lessons/RobotManager.java
@@ -0,0 +1,26 @@
+package Lessons;
+
+
+
+public class RobotManager
+{
+    public static void main(String[] args) {
+        Robot robot1 = new Robot("Test Robot 1");
+        Robot robot2 = new Robot("Test Robot 2");
+        System.out.println(robot1.toString());
+        System.out.println(robot2.toString());
+    }
+}
+class Robot
+{
+    private String name;
+
+    public Robot(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "name = " + name;
+    }
+}
