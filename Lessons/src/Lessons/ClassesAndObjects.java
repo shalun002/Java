8c956b6c28cc31c49150dd41a5a24a61df2059c9
shalun002 <32149071+shalun002@users.noreply.github.com>
2018-04-17 10:18:54 +0600

    ClassesAndObjects

--- Lessons/src/Lessons/ClassesAndObjects.java
+++ Lessons/src/Lessons/ClassesAndObjects.java
@@ -3,18 +3,48 @@ package Lessons;
 public class ClassesAndObjects {
     public static void main(String[] args) {
         Person person = new Person();
-        person.name = "Sergey";
-        person.age = 32;
-        person.speak();
-        person.sayHello();
-        System.out.println();
+        person.setName("Roma");
+        person.setAge(5);
+
+        System.out.println("Name: " + person.getName() + ", " + "age: " + person.getAge());
     }
 }
 
 class Person{
-   String name;
-   int age;
+   private String name;
+   private int age;
 
+
+   public void setName(String userName){
+       if(userName.isEmpty()){
+           System.out.println("Error!");
+       }
+       else {
+           name = userName;
+       }
+   }
+
+   public String getName(){
+       return name;
+   }
+
+    public void setAge(int userAge){
+        if (userAge < 0){
+            System.out.println("Error!");
+        }
+        else {
+            age = userAge;
+        }
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+   int calculateYearsToRetirement(){
+       int years = 65 - age;
+       return years;
+   }
    void speak(){
        for (int i = 0; i < 3; i++) {
            System.out.println("Имя " + name + ", " + "возраст " + age + " года");
@@ -23,5 +53,5 @@ class Person{
 
    void sayHello(){
        System.out.println("Hello");
-    }
+   }
 }
