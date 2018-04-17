10072af0944701e3b80f0f048243ce3efeb5ead8
shalun002 <32149071+shalun002@users.noreply.github.com>
2018-04-17 18:15:50 +0600

    Static

--- Lessons/src/Lessons/Static.java
+++ Lessons/src/Lessons/Static.java
@@ -2,6 +2,29 @@ package Lessons;
 
 public class Static {
     public static void main(String[] args) {
+        System.out.println(Math.mult(12, 3));
+        System.out.println(Math.summ(25, 2));
+        //System.out.println(Math.x);
+
+        Math math = new Math();
+        int newX = math.x * 3;
+
+        System.out.println(newX);
+        System.out.println(math.x);
 
     }
 }
+
+class Math {
+
+    public static int x = 12;
+
+    public static int summ(int a, int b) {
+        return a + b;
+    }
+
+    public static int mult(int a, int b) {
+        return a * b;
+    }
+}
+
