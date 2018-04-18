29960bedc8de93cd933ab4170cc055721cd23563
shalun002 <32149071+shalun002@users.noreply.github.com>
2018-04-18 10:32:13 +0600

    Inheritance

---
+++ Lessons/src/Lessons/Inheritance.java
@@ -0,0 +1,16 @@
+package Lessons;
+
+public class Inheritance {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.eat();
+        animal.sleep();
+
+        System.out.println();
+
+        Dog dog = new Dog();
+        dog.eat();
+        dog.sleep();
+        dog.bark();
+    }
+}
