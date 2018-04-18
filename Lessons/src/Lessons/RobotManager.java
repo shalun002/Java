176beb4eb6d9a10afe0b43f06c5d4caede9e28af
shalun002 <32149071+shalun002@users.noreply.github.com>
2018-04-18 10:10:08 +0600

    RobotManager

--- Lessons/src/Lessons/RobotManager.java
+++ Lessons/src/Lessons/RobotManager.java
@@ -7,8 +7,8 @@ public class RobotManager
     public static void main(String[] args) {
         Robot robot1 = new Robot("Test Robot 1");
         Robot robot2 = new Robot("Test Robot 2");
-        System.out.println(robot1.toString());
-        System.out.println(robot2.toString());
+        System.out.println(robot1);
+        System.out.println(robot2);
     }
 }
 class Robot
