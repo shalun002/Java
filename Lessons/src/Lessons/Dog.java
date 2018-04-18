087ef47ff5b52ea35b6cff4e6cb15e565e3e2fe7
shalun002 <32149071+shalun002@users.noreply.github.com>
2018-04-18 10:32:27 +0600

    Dog

---
+++ Lessons/src/Lessons/Dog.java
@@ -0,0 +1,13 @@
+package Lessons;
+
+public class Dog extends Animal {
+
+    @Override
+    public void eat(){
+        System.out.println("Dog is eating");
+    }
+    public void bark(){
+        System.out.println("I am barking");
+    }
+
+}
